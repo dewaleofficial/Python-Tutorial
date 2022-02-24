f452fe697d73030f18aca2826bdd24a6c1b3174c
Abdul-Fattah99 <qabdulfatai@gmail.com>
2022-02-24 14:12:01 +0100

    main py file for execution and testing of classes

--- classes.py
+++ classes.py
@@ -1,70 +1,11 @@
-import csv
+from all_items import items
+from phones import Phone
 
-class items():
-    
-    #create a list of items for all stores
-    all=[]
+phone1 = Phone("olx", "samsung", 100, 20)
 
-    pay_rate = 0.8 #class/global attribute(discount of 20% across all sales)
+phone2 = Phone("jumia", "infinix", 80, 20)
 
-
-    def __init__(self,store: str, name: str, price: float, quantity:int,  withInk = False):
-
-        #validate values being inserted 
-        assert price >= 0, f"Price {price} is not greater than or equal to zero"
-        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero"
-
-        # assign int values
-        self.store = store
-        self.name = name
-        self.price=price
-        self.quantity = quantity
-        self.withInk = withInk
-
-        #everytime an instance is created we want to append to the list
-        #it should be in the init function because the init function is called once the class is loaded
-    
-        items.all.append(self)        
-
-    def calculate_total_price(self):
-        return self.price*self.quantity
-
-    def apply_discount(self):
-        self.price = self.price*self.pay_rate
-        return self.price
-
-    #create a class method to connect to csv and instantiate some objects
-    @classmethod
-    def instanciate_from_csv(cls):
-        with open('items.csv', 'r') as f:
-            reader = csv.DictReader(f)
-            itemly = list(reader)
-
-        for item in itemly:
-            items(
-                store=item.get('store'),
-                name=item.get('name'),
-                price=item.get('price'),
-                quantity=item.get('quantity'),
-                withInk=item.get('withInk')
-            )
-
-
-    #static method
-    @staticmethod
-    def isnumber(num):#can take any argument
-    # we want to count out the float that are point 0 i.e 5.0, 10.0
-        if isinstance(num,float):
-            return num.is_integer()
-        elif isinstance(num,int):
-            return True
-        else:
-            return False
-
-
-    #allows you vew your instances an an object
-    def __repr__(self):
-        return f"items('{self.store}', '{self.name}','{self.price}', '{self.quantity}', '{self.withInk}')"
+print(phone2.apply_discount())
 
 
 #item1 = items("aliexpress", "book", 100, 10)
@@ -102,5 +43,5 @@ class items():
 #item1.apply_discount()
 #print(item1.price)
 
-items.instanciate_from_csv()
-print(items.all)
+#items.instanciate_from_csv()
+#print(items.all)
