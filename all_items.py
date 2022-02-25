a8ef8810dc65f4028f0f5c4f0083dd4e9cb3ec14
Abdul-Fattah99 <qabdulfatai@gmail.com>
2022-02-25 14:48:57 +0100

    encapsulation, abstraction, polymorphism, and inheritance explained

--- all_items.py
+++ all_items.py
@@ -16,8 +16,9 @@ class items():
 
         # assign int values
         self.store = store
-        self.name = name
-        self.price=price
+        #add double under score to block access from outside source(make private)
+        self.__name = name
+        self.__price=price
         self.quantity = quantity
         self.withInk = withInk
 
@@ -26,12 +27,31 @@ class items():
     
         items.all.append(self)        
 
+    @property
+    #read-only attribute
+    def name(self):
+        print("you are trying to get name")
+        return self.__name
+
+# set new nvalue for name
+    @name.setter
+    def name(self, value):
+        if len(value)>10:
+            raise Exception("name too long")
+        else:
+            self.__name = value
+
+#lets enapsulate the price attribute
+    @property
+    def price(self):
+        return self.__price
+
     def calculate_total_price(self):
-        return self.price*self.quantity
+        return self.__price*self.quantity
 
     def apply_discount(self):
-        self.price = self.price*self.pay_rate
-        return self.price
+        self.__price = self.__price*self.pay_rate
+        return self.__price
 
     #create a class method to connect to csv and instantiate some objects
     @classmethod
@@ -64,4 +84,22 @@ class items():
 
     #allows you vew your instances an an object
     def __repr__(self):
-        return f"items('{self.store}', '{self.name}','{self.price}', '{self.quantity}', '{self.withInk}')"
+        return f"items('{self.store}', '{self.name}','{self.__price}', '{self.quantity}', '{self.withInk}')"
+
+#ABSTRACTION  - Show only important attribute and hide unnecessary information
+#make the methods private and unaccessible outside this class 
+    def __connect(self, smpt_server):
+        pass
+
+    def __prepare_body(self):
+        return f"""
+            You have {self.name} at price {self.price}
+        """
+        pass
+    def __send(self):
+        pass
+
+    def send_email(self):
+        self.__conect()
+        self.__prepare_body()
+        self.__send()
--- classes.py
+++ classes.py
@@ -5,9 +5,18 @@ phone1 = Phone("olx", "samsung", 100, 20)
 
 phone2 = Phone("jumia", "infinix", 80, 20)
 
-print(phone2.apply_discount())
+#print(phone2.apply_discount())
+
+item1 = items("olx", "samsung", 100, 20)
+item1.name = "others"
+#item1.price = 1000
+item1.apply_discount()
+
+print(item1.name)
+print(item1.price)
 
 
+item1.send_email()
 #item1 = items("aliexpress", "book", 100, 10)
 #item2 = items("olx", "pencil", 200, 10 )
 #tem3 = items("jumia", "pen", 300, 10, True )
