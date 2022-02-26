27eb8da7690b7c412fe24276e848c3e8b4bf4f73
Abdul-Fattah99 <qabdulfatai@gmail.com>
2022-02-26 23:01:39 +0100

    optimized thte phone class

--- phones.py
+++ phones.py
@@ -1,7 +1,10 @@
+#import class items for file all_items
 from all_items import items
 
+# NEW CLASS PHONE TO INHERIT FROM items class
 class Phone(items):
     
+    #initialize the attrbutes, include any new attributes specific to this class ie isBroken
     def __init__(self,store: str, name: str, price: float, quantity:int,  withInk = False, isBroken = 0):
     
         #super function allows us have access to all the attributes of the parent class
@@ -13,5 +16,4 @@ class Phone(items):
         assert isBroken >= 0, f"isBroken {isBroken} is not greater than or equal to zero"
 
         # assign self object
-       
         self.isBroken = isBroken
